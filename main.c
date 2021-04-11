4e1884d99f19815eb4a53bfe9c9947e1dc1efe66
Dixinlei <82302519+Dixinlei@users.noreply.github.com>
2021-04-11 13:13:02 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+	printf("这是我的第一个c语言程序\n");
+	printf("我学习了#include是预处理器\n");
+	printf("main是主函数不能缺少所有的程序都是由main开始的\n"); 
+	printf("printf是输出语句\n"); 
+    return 0;
+}
